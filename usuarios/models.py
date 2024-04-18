74adb30403a82a716b8b518bcf38ad9b4dcb2751
DarkKing516 <jhomai7020@gmail.com>
2024-04-17 22:44:17 -0500

    inicial

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Rol(models.Model):
+    nombre_rol = models.CharField(max_length=50)
+    estado_rol = models.CharField(max_length=1, default='A')
+
+    def __str__(self):
+        return self.nombre_rol
+
+class Permiso(models.Model):
+    nombre_permiso = models.CharField(max_length=50)
+    estado_permiso = models.CharField(max_length=1, default='A')
+
+    def __str__(self):
+        return self.nombre_permiso
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=15)
+    documento = models.CharField(max_length=15)
+    correo = models.EmailField()
+    usuario = models.CharField(max_length=20)
+    contraseña = models.CharField(max_length=255)
+    estado = models.CharField(max_length=1, default='A')
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class RolxPermiso(models.Model):
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    permiso = models.ForeignKey(Permiso, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.rol} - {self.permiso}"
