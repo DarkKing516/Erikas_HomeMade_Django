2c41071ca5a8924a0ee0f893389ec9ce5f624f17
DarkKing516 <jhomai7020@gmail.com>
2024-04-17 21:56:03 -0500

    inicial

--- Erikas_HomeMade/settings.py
+++ Erikas_HomeMade/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'Erikas_HomeMade.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'erikas_homemade',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # O la dirección IP de tu servidor MySQL
+        'PORT': '3306',  # El puerto por defecto de MySQL
     }
 }
 
