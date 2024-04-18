f35462ad5c003d3305e63de8fac69b99718dd160
unknown <Sena@MEDAPRCSGFSD729.FormacionSena.RED>
2024-04-18 15:49:38 -0500

    se genera la base de datos ugu

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Rol
+from .models import *
+from .forms import RolForm
 
 # Create your views here.
 def hello(request):
@@ -9,3 +10,24 @@ def hello(request):
 def listar_roles(request):
     roles = Rol.objects.all()
     return render(request, 'listar_roles.html', {'roles': roles})
+
+def crear_rol(request):
+    if request.method == 'POST':
+        form = RolForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_roles')
+    else:
+        form = RolForm()
+    return render(request, 'crear_rol.html', {'form': form})
+
+def editar_rol(request, id_rol):
+    rol = get_object_or_404(Rol, pk=id_rol)
+    if request.method == 'POST':
+        form = RolForm(request.POST, instance=rol)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_roles')
+    else:
+        form = RolForm(instance=rol)
+    return render(request, 'editar_rol.html', {'form': form})
