d39a99759f741b78297499333c5db760171f1b3c
unknown <Sena@MEDAPRCSGFSD729.FormacionSena.RED>
2024-04-24 15:35:42 -0500

    se añaden las ventas uwu

--- usuarios/signals.py
+++ usuarios/signals.py
@@ -6,6 +6,6 @@ from .models import Rol
 @receiver(pre_delete, sender=Rol)
 def prevent_deletion_of_special_roles(sender, instance, **kwargs):
     # Verificar si el ID del rol es 0 o 1
-    if instance.pk in [0, 1]:
+    if instance.pk in [1, 2]:
         # Generar una excepción o un mensaje de error para evitar la eliminación del rol
         raise PermissionDenied("No se puede eliminar este rol.")
