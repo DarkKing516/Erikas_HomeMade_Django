20d18b8e7df0d5af3809b1446d8e1080a2700ba2
DarkKing516 <jhomai7020@gmail.com>
2024-04-29 16:58:06 -0500

    ventas medio terminado

--- ventas/views.py
+++ ventas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Venta
 from .forms import VentaForm
+from pedidos.models import Pedido
 
 def listar_ventas(request):
     ventas = Venta.objects.all()
@@ -11,7 +12,13 @@ def crear_venta(request):
     if request.method == 'POST':
         form = VentaForm(request.POST)
         if form.is_valid():
-            form.save()
+            venta = form.save(commit=False)
+            pedido_id = form.cleaned_data['idPedido'].idPedido  # Obtener el ID del pedido
+            pedido = get_object_or_404(Pedido, idPedido=pedido_id)
+            venta.subtotal = pedido.subtotal
+            venta.iva = pedido.iva
+            venta.total = form.cleaned_data['total']  # Usar el total proporcionado en el formulario
+            venta.save()
             return redirect('ventas:listar_ventas') 
     else:
         form = VentaForm()
