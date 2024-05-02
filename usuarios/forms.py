d529a9ef0bba121157e6fb048f0f768a08026a1b
unknown <Sena@MEDAPRCSGFSD729.FormacionSena.RED>
2024-05-02 14:52:08 -0500

    avances usuarios

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -75,8 +75,19 @@ class RegistroForm(forms.ModelForm):
             'usuario': forms.TextInput(attrs={'class': 'inputField', 'placeholder': 'Usuario'}),
             'contraseña': forms.PasswordInput(attrs={'class': 'inputField', 'placeholder': 'Contraseña'}),
         }
-        
-        
+    def clean_documento(self):
+        documento = self.cleaned_data['documento']
+        # Validar si el documento contiene solo números
+        if not documento.isdigit():
+            raise forms.ValidationError("El documento debe contener solo números.")
+        return documento
+    def clean_telefono(self):
+        telefono = self.cleaned_data['telefono']
+        # Validar si el teléfono contiene solo números
+        if not telefono.isdigit():
+            raise forms.ValidationError("El teléfono debe contener solo números.")
+        return telefono
+
 
 class CreateUsuario(forms.ModelForm):
     nombre = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}))
@@ -89,3 +100,17 @@ class CreateUsuario(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ['idRol', 'nombre', 'telefono', 'documento', 'correo', 'usuario', 'contraseña']
+        
+    def clean_documento(self):
+        documento = self.cleaned_data['documento']
+        # Validar si el documento contiene solo números
+        if not documento.isdigit():
+            raise forms.ValidationError("El documento debe contener solo números.")
+        return documento
+
+    def clean_telefono(self):
+        telefono = self.cleaned_data['telefono']
+        # Validar si el teléfono contiene solo números
+        if not telefono.isdigit():
+            raise forms.ValidationError("El teléfono debe contener solo números.")
+        return telefono
