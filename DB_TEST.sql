58003034475b29335cddca4b4875c70c94c1b63a
unknown <Sena@MEDAPRCSGFSD729.FormacionSena.RED>
2024-05-06 16:51:40 -0500

    se hacen validaciones en reservas y usuarios y ventas

--- DB_TEST.sql
+++ DB_TEST.sql
@@ -1,5 +1,3 @@
-START TRANSACTION;
-
 -- Insertar 20 permisos de ejemplo
 INSERT INTO permisos (nombre_permiso, estado_permiso) VALUES
 ('Listar Usuarios', 'activo'),
@@ -10,7 +8,10 @@ INSERT INTO permisos (nombre_permiso, estado_permiso) VALUES
 ('Listar Mis Reservas', 'activo'),
 ('Crear Reservas', 'activo'),
 ('Editar Reservas', 'activo'),
-('Eliminar Reservas', 'activo');
+('Eliminar Reservas', 'activo'),
+('Listar Ventas', 'activo'),
+('Crear Ventas', 'activo'),
+('Listar Mis Ventas', 'activo');
 
 -- Insertar roles
 INSERT INTO roles (nombre_rol, estado_rol) VALUES
@@ -42,34 +43,3 @@ INSERT INTO `reservas` (`fecha`, `fecha_cita`, `descripcion`, `estado`, `usuario
 ('2024-05-05 15:36:33.000000', '2024-05-07 15:36:33.000000', 'Prueba 1', 'Completada', '1'),
 ('2024-05-05 15:36:33.000000', '2024-05-07 15:36:33.000000', 'Prueba 1', 'En Proceso', '2'),
 ('2024-05-05 15:36:33.000000', '2024-05-07 15:36:33.000000', 'Prueba 1', 'Completada', '1');
-
-
--- Insertar tipos de productos de ejemplo
-INSERT INTO tipo_productos (nombre_producto, estado_producto) VALUES
-('Electrónica', 'activo'),
-('Ropa', 'activo'),
-('Alimentos', 'activo'),
-('Hogar', 'inactivo');
-
--- Insertar productos de ejemplo
-INSERT INTO productos (nombre, descripcion, imagen, precio, estado_producto, estado_catalogo, cantidad, id_TipoProducto_id) VALUES
-('Teléfono móvil', 'Teléfono móvil de última generación', 'telefono.jpg', 699.99, 'activo', 'disponible', 50, 1),
-('Camiseta', 'Camiseta de algodón de color blanco', 'camiseta.jpg', 19.99, 'activo', 'disponible', 100, 2),
-('Arroz', 'Arroz blanco de grano largo', 'arroz.jpg', 2.99, 'activo', 'disponible', 200, 3),
-('Lámpara', 'Lámpara de mesa con pantalla de tela', 'lampara.jpg', 39.99, 'activo', 'disponible', 30, 4);
-
--- Pedidos
-INSERT INTO pedidos (fechaCreacion_pedido, fecha_pedido, descripcion_pedido, subtotal, iva, total, evidencia_pago, estado_pedido, id_Usuario_id) VALUES
-('2024-04-23 08:00:00', '2024-04-25', 'Pedido para Juan Perez', 1459.95, 0, 1459.95, 'comprobante_pago.jpg', 'pendiente', 1),
-('2024-04-24 09:00:00', '2024-04-26', 'Pedido para María Lopez', 62.96, 0, 62.96, 'comprobante_pago2.jpg', 'pagado', 2);
-
-
--- Para la tabla detalle_pedido_productos:
-
-INSERT INTO detalle_pedido_productos (cant_productos, nombre_productos, descripcion, precio_inicial_producto, subtotal_productos, idProducto_id, idPedido_id) VALUES
-(2, 'Teléfono móvil', 'Teléfono móvil de última generación', 699.99, 1399.98, 1, 1),
-(3, 'Camiseta', 'Camiseta de algodón de color blanco', 19.99, 59.97, 2, 1),
-(1, 'Arroz', 'Arroz blanco de grano largo', 2.99, 2.99, 3, 2),
-(2, 'Lámpara', 'Lámpara de mesa con pantalla de tela', 39.99, 79.98, 4, 2);
-
-COMMIT;
