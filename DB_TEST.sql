c59a9905fc4ee63fc67e9f99885245ca19532064
DarkKing516 <jhomai7020@gmail.com>
2024-05-06 20:42:07 -0500

    validaciones en nav

--- DB_TEST.sql
+++ DB_TEST.sql
@@ -3,11 +3,43 @@ INSERT INTO permisos (nombre_permiso, estado_permiso) VALUES
 ('Listar Usuarios', 'activo'),
 ('Crear Usuarios', 'activo'),
 ('Editar Usuarios', 'activo'),
+('Editar Perfil', 'activo'),
 ('Eliminar Usuarios', 'activo'),
+('Listar Roles', 'activo'),
+('Crear Roles', 'activo'),
+('Editar Roles', 'activo'),
+('Eliminar Roles', 'activo'),
+('Listar Permisos', 'activo'),
+('Crear Permisos', 'activo'),
+('Editar Permisos', 'activo'),
+('Eliminar Permisos', 'activo'),
+('Listar Productos', 'activo'),
+('Crear Productos', 'activo'),
+('Editar Productos', 'activo'),
+('Eliminar Productos', 'activo'),
+('Listar Tipo Productos', 'activo'),
+('Crear Tipo Productos', 'activo'),
+('Editar Tipo Productos', 'activo'),
+('Eliminar Tipo Productos', 'activo'),
+('Listar Servicios', 'activo'),
+('Crear Servicios', 'activo'),
+('Editar Servicios', 'activo'),
+('Eliminar Servicios', 'activo'),
+('Listar Tipo Servicios', 'activo'),
+('Crear Tipo Servicios', 'activo'),
+('Editar Tipo Servicios', 'activo'),
+('Eliminar Tipo Servicios', 'activo'),
+('Listar Pedidos', 'activo'),
+('Listar Mis Pedidos', 'activo'),
+('Crear Pedidos', 'activo'),
+('Editar Pedidos', 'activo'),
+('Editar Mis Pedidos', 'activo'),
+('Eliminar Pedidos', 'activo'),
 ('Listar Reservas', 'activo'),
 ('Listar Mis Reservas', 'activo'),
 ('Crear Reservas', 'activo'),
 ('Editar Reservas', 'activo'),
+('Editar Mis Reservas', 'activo'),
 ('Eliminar Reservas', 'activo'),
 ('Listar Ventas', 'activo'),
 ('Crear Ventas', 'activo'),
@@ -21,12 +53,26 @@ INSERT INTO roles (nombre_rol, estado_rol) VALUES
 ('Analista', 'I'),
 ('Soporte Técnico', 'I');
 
--- Insertar permisos por rol
+-- Insertar permisos para el rol Admin
 INSERT INTO roles_permisos (rol_id, permiso_id) 
 SELECT r.id AS rol_id, p.id AS permiso_id 
 FROM roles r 
 CROSS JOIN permisos p 
 WHERE r.nombre_rol = 'Admin'
+AND NOT EXISTS (
+    SELECT 1 FROM roles_permisos rp 
+    WHERE rp.rol_id = r.id AND rp.permiso_id = p.id
+);
+-- Insertar permisos por rol para el rol de Cliente
+INSERT INTO roles_permisos (rol_id, permiso_id) 
+SELECT r.id AS rol_id, p.id AS permiso_id 
+FROM roles r 
+CROSS JOIN permisos p 
+WHERE r.nombre_rol = 'Cliente'
+AND (
+    p.nombre_permiso LIKE '%Mis%' 
+    OR p.nombre_permiso = 'Editar Perfil'
+)
 AND NOT EXISTS (
     SELECT 1 FROM roles_permisos rp 
     WHERE rp.rol_id = r.id AND rp.permiso_id = p.id
