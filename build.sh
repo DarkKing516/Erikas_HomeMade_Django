bcc792684bca7798c43b70626dfd75b89f78843e
DarkKing516 <jhomai7020@gmail.com>
2024-07-22 14:40:58 -0500

    comentario em build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-# python manage.py collectstatic --no-input || true
+python manage.py collectstatic --no-input || true
 
 # Apply any outstanding database migrations
 # python manage.py makemigrations
