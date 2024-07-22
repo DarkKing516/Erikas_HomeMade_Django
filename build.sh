eb7ea1d38e443638bbdaf31e20706ef4b30e3873
DarkKing516 <jhomai7020@gmail.com>
2024-07-22 16:58:16 -0500

    ahora si deberia funcionar lo de la factura

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Update package list and install dependencies
+apt-get update
+apt-get install -y software-properties-common
+apt-get install -y wkhtmltopdf
+
+# Install Python dependencies
 pip install -r requirements.txt
 
-# Convert static asset files
-apt-get install -y wkhtmltopdf
+# Collect static files
 python manage.py collectstatic --no-input || true
 
 # Apply any outstanding database migrations
