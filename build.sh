5ce2c404bc1bd209877b43c610b474b05c523411
DarkKing516 <jhomai7020@gmail.com>
2024-07-23 14:12:43 -0500

    se comenta lo del build otra ve

--- build.sh
+++ build.sh
@@ -10,20 +10,20 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Update package list and install dependencies
-apt-get update
-apt-get install -y software-properties-common
-apt-get install -y wkhtmltopdf
+# # Update package list and install dependencies
+# apt-get update
+# apt-get install -y software-properties-common
+# apt-get install -y wkhtmltopdf
 
-# Check if wkhtmltopdf is installed and accessible
-if ! command -v wkhtmltopdf &> /dev/null
-then
-    echo "wkhtmltopdf could not be found"
-    exit
-fi
+# # Check if wkhtmltopdf is installed and accessible
+# if ! command -v wkhtmltopdf &> /dev/null
+# then
+#     echo "wkhtmltopdf could not be found"
+#     exit
+# fi
 
 # Print the path to wkhtmltopdf
-which wkhtmltopdf
+# which wkhtmltopdf
 
 # Collect static files
 python manage.py collectstatic --no-input || true
