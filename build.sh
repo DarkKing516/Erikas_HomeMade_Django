0779b0ddc4a33ff72ff03b98c87d5d193b128d6e
DarkKing516 <jhomai7020@gmail.com>
2024-07-23 11:06:47 -0500

    prueba coso factura

--- build.sh
+++ build.sh
@@ -10,10 +10,15 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Download and install wkhtmltopdf
-curl -L -o wkhtmltopdf.deb https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
-dpkg -i wkhtmltopdf.deb
-apt-get install -f  # Install any dependencies
+# Download and extract portable wkhtmltopdf
+curl -L -o wkhtmltopdf.tar.xz https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.bionic_amd64.tar.xz
+tar -xf wkhtmltopdf.tar.xz
+
+# Move the binaries to a directory included in PATH
+mv wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
+
+# Clean up
+rm -rf wkhtmltopdf.tar.xz wkhtmltox
 
 # Collect static files
 python manage.py collectstatic --no-input || true
