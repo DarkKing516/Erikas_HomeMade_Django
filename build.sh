668ff0aa851ed936ce777594979545321384f089
DarkKing516 <jhomai7020@gmail.com>
2024-07-23 11:14:06 -0500

    prueba coso factura

--- build.sh
+++ build.sh
@@ -10,15 +10,18 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
+# Create a directory for wkhtmltopdf in the home directory
+mkdir -p $HOME/bin
+
 # Download and extract portable wkhtmltopdf
-curl -L -o wkhtmltopdf.tar.xz https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
-dpkg -x wkhtmltopdf.tar.xz wkhtmltopdf
+curl -L -o wkhtmltopdf.deb https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+dpkg -x wkhtmltopdf.deb wkhtmltopdf
 
 # Move the binaries to a directory included in PATH
-cp wkhtmltopdf/usr/local/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
+cp wkhtmltopdf/usr/local/bin/wkhtmltopdf $HOME/bin/wkhtmltopdf
 
 # Clean up
-rm -rf wkhtmltopdf.tar.xz wkhtmltopdf
+rm -rf wkhtmltopdf.deb wkhtmltopdf
 
 # Collect static files
 python manage.py collectstatic --no-input || true
