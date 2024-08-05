9a8ee7db5a1ceb6daf417f51dc941f568243e0d7
DarkKing516 <jhomai7020@gmail.com>
2024-08-05 16:49:22 -0500

    cambios en API para el editar en flutter

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -1,6 +1,8 @@
 # usuarios/serializers.py
 from rest_framework import serializers
 from .models import Usuario, Rol, Permiso
+from django.contrib.auth.hashers import make_password
+
 
 class PermisoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,7 +18,20 @@ class RolSerializer(serializers.ModelSerializer):
 
 class UsuarioSerializer(serializers.ModelSerializer):
     idRol = RolSerializer(read_only=True)
-
+    
     class Meta:
         model = Usuario
         fields = '__all__'
+        extra_kwargs = {'contraseña': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['contraseña'] = make_password(validated_data.get('contraseña'))
+        return super(UsuarioSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'contraseña' in validated_data and validated_data['contraseña']:
+            validated_data['contraseña'] = make_password(validated_data['contraseña'])
+        else:
+            # Keep the existing password if new password is not provided
+            validated_data.pop('contraseña', None)
+        return super(UsuarioSerializer, self).update(instance, validated_data)
