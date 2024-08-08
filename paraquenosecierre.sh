5acc3376d1ddf976894c9cba1059dafc1e05ba0a
DarkKing516 <jhomai7020@gmail.com>
2024-08-08 15:42:33 -0500

    EEEEEEEEEEEEEEEEEEE

--- paraquenosecierre.sh
+++ paraquenosecierre.sh
@@ -3,7 +3,7 @@
 while true
 do
     # Realiza una solicitud HTTP a tu aplicación
-    curl https://tu-app-en-render.com
+    curl https://erikas-homemade.onrender.com/
     
     # Espera 40 minutos antes de hacer la siguiente solicitud
     sleep 2400
