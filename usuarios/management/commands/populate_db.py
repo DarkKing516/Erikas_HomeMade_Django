468c17745a7dccfd2420fc224cef1ccfe6631379
DarkKing516 <jhomai7020@gmail.com>
2024-09-03 14:54:45 -0500

    dejo todo listoo en caso de que se necesite migrar a una nueva db

--- usuarios/management/commands/populate_db.py
+++ usuarios/management/commands/populate_db.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from usuarios.models import Permiso, Rol, Usuario
+from tqdm import tqdm  # Importamos tqdm para la barra de progreso
 
 class Command(BaseCommand):
     help = 'Popula la base de datos con permisos, roles y usuarios'
@@ -23,11 +24,13 @@ class Command(BaseCommand):
             'Editar Evidencia Pedidos', 'Editar Estado Tipo Productos', 'Editar Estado Productos', 
             'Editar Estado Catalogo Productos', 'Cambiar Imagen Productos', 
             'Cambiar Imagen Servicios', 'Editar Estado Servicio Catalogo', 'Editar Estado Servicio', 
-            'Editar Estado Tipo Servicios'
+            'Editar Estado Tipo Servicios', 'Listar Dashboard'
         ]
 
+        # Barra de progreso para insertar permisos
+        self.stdout.write('Insertando permisos...')
         permiso_objs = []
-        for nombre_permiso in permisos:
+        for nombre_permiso in tqdm(permisos, desc='Progreso permisos'):
             try:
                 # Eliminar permisos duplicados
                 Permiso.objects.filter(nombre_permiso=nombre_permiso).delete()
@@ -39,35 +42,49 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.ERROR(f"Multiple entries found for {nombre_permiso}, please check your data."))
 
         # Insertar roles
+        self.stdout.write('Insertando roles...')
         rol1, created = Rol.objects.get_or_create(nombre_rol='Admin')
         rol2, created = Rol.objects.get_or_create(nombre_rol='Cliente')
+        rol3, created = Rol.objects.get_or_create(nombre_rol='Testing')
 
-        # Asignar permisos a roles
-        rol1.permisos.set(permiso_objs)
+        # Permisos del cliente
+        permisos_cliente = [
+            'Editar Perfil', 'Listar Mis Pedidos', 'Editar Mis Pedidos', 
+            'Listar Mis Reservas', 'Editar Mis Reservas', 'Listar Mis Ventas'
+        ]
+
+        # Barra de progreso para asignar permisos a roles
+        self.stdout.write('Asignando permisos a roles...')
+        permisos_cliente_objs = Permiso.objects.filter(nombre_permiso__in=permisos_cliente)
+        rol2.permisos.set(permisos_cliente_objs)
+
+        permisos_admin = Permiso.objects.exclude(nombre_permiso__icontains='Eliminar').exclude(nombre_permiso__in=permisos_cliente)
+        rol1.permisos.set(permisos_admin)
         
-        # Insertar o actualizar usuarios
-        usuario1, created = Usuario.objects.update_or_create(
-            correo='jhomai7020@gmail.com',
-            defaults={
-                'idRol': rol1,
-                'nombre': 'Admin User',
-                'telefono': '1234567890',
-                'documento': '987654321',
-                'usuario': 'adminuser',
-                'contraseña': '1234'
-            }
-        )
+        rol3.permisos.set(permiso_objs)
 
-        usuario2, created = Usuario.objects.update_or_create(
-            correo='use1r@gmail.com',
-            defaults={
-                'idRol': rol2,
-                'nombre': 'Regular User',
-                'telefono': '0987654321',
-                'documento': '123456789',
-                'usuario': 'regularuser',
-                'contraseña': 'userpassword'
-            }
-        )
+        # Barra de progreso para insertar o actualizar usuarios
+        self.stdout.write('Insertando o actualizando usuarios...')
+        usuarios = [
+            {'correo': 'admin@gmail.com', 'rol': rol1, 'nombre': 'Admin User', 'telefono': '1234567890', 'documento': '987654321', 'usuario': 'adminuser', 'contraseña': 'pbkdf2_sha256$720000$zMcWkNN21PjxTopxovtIhQ$sDU661j/jTud46bpOiKICosHW8IQGtCdsU/EursOcFU='},
+            {'correo': 'cliente@gmail.com', 'rol': rol2, 'nombre': 'Regular User', 'telefono': '0987654321', 'documento': '123456789', 'usuario': 'regularuser', 'contraseña': 'pbkdf2_sha256$720000$zMcWkNN21PjxTopxovtIhQ$sDU661j/jTud46bpOiKICosHW8IQGtCdsU/EursOcFU='},
+            {'correo': 'jhomai7020@gmail.com', 'rol': rol3, 'nombre': 'Jhon', 'telefono': '3213309427', 'documento': '1047173611', 'usuario': 'Jhon Connor', 'contraseña': 'pbkdf2_sha256$720000$zMcWkNN21PjxTopxovtIhQ$sDU661j/jTud46bpOiKICosHW8IQGtCdsU/EursOcFU='},
+            {'correo': 'danilovergara257@gmail.com', 'rol': rol3, 'nombre': 'Danilo Vergara', 'telefono': '3177099118', 'documento': '104123613', 'usuario': 'Dan', 'contraseña': 'pbkdf2_sha256$720000$zMcWkNN21PjxTopxovtIhQ$sDU661j/jTud46bpOiKICosHW8IQGtCdsU/EursOcFU='},
+            {'correo': 'juanfelipesanchezsanchezherrer@gmail.com', 'rol': rol3, 'nombre': 'Emmanuel Sanchez', 'telefono': '3203097136', 'documento': '1073973612', 'usuario': 'Pelos', 'contraseña': 'pbkdf2_sha256$720000$zMcWkNN21PjxTopxovtIhQ$sDU661j/jTud46bpOiKICosHW8IQGtCdsU/EursOcFU='},
+            {'correo': 'valenciaalzatenelsondavid@gmail.com', 'rol': rol3, 'nombre': 'Nelson David', 'telefono': '3146060549', 'documento': '104273611', 'usuario': 'Murcia', 'contraseña': 'pbkdf2_sha256$720000$zMcWkNN21PjxTopxovtIhQ$sDU661j/jTud46bpOiKICosHW8IQGtCdsU/EursOcFU='}
+        ]
+
+        for user_data in tqdm(usuarios, desc='Progreso usuarios'):
+            Usuario.objects.update_or_create(
+                correo=user_data['correo'],
+                defaults={
+                    'idRol': user_data['rol'],
+                    'nombre': user_data['nombre'],
+                    'telefono': user_data['telefono'],
+                    'documento': user_data['documento'],
+                    'usuario': user_data['usuario'],
+                    'contraseña': user_data['contraseña']
+                }
+            )
 
         self.stdout.write(self.style.SUCCESS('Datos insertados o actualizados correctamente.'))
